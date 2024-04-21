cab2e9e31ec566e98197bd117b38cf5577a199d3
Barbora Fabianova <BARBORKA.FABIANOVA@GMAIL.COM>
2024-04-21 15:31:35 +0000

    Deployment to Heroku

--- run.py
+++ run.py
@@ -1,9 +1,11 @@
 import os   # standard python library
 import json # standard json library because we have company.js file which will be used in about.html page
-from flask import Flask, render_template #capital indicates that it is a class name, import as well render function so that I do not need to type html code
-
+from flask import Flask, render_template, request, flash #capital indicates that it is a class name, import as well render function so that I do not need to type html code. Request library is for the contact form, flash is for showing the feedback to the user. Flash message will dissapear from the screen after refresh. To sue flashed message we need to create a secret key
+if os.path.exists("env.py"):
+    import env
 
 app = Flask(__name__)  #in Flask the variable is called app
+app.secret_key= os.environ.get("SECRET_KEY")
 
 
 @app.route("/") #Then, we use the route decorator to tell Flask what URL should trigger the function that follows.
@@ -16,11 +18,24 @@ def about():
     data = [] # I initialize here the new empty list called data
     with open("data/company.json", "r") as json_data:  # I need to initialize python to open json file as read only and assign this file to a new variable json_data
         data = json.load(json_data)
-    return render_template("about.html", page_title="About", company=data)
+    return render_template("about.html", page_title="About", company=data) #company is the company.json file where we pass the data list variable
+
+@app.route("/about/<member_name>") #this is rooting decorater for company.json names and url links, which is used on the about.html page
+def about_member(member_name):      #this is a view for the rooting decorator
+    member = {}                        #this is empty object 
+    with open("data/company.json", "r") as json_data:
+        data=json.load(json_data)
+        for obj in data:
+            if obj["url"]== member_name:
+                member = obj            #object is pushed to member object
+        return render_template("member.html" , member = member) # to member.html is passed the object member
 
 
-@app.route("/contact")
+@app.route("/contact", methods=["GET", "POST"])
 def contact():
+    if request.method == "POST":
+        flash("Thanks {}, we have received your message!".format(
+            request.form.get("name")))
     return render_template("contact.html", page_title="Contact")
 
 
